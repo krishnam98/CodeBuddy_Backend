ebb38829aa34df23658e538b8965ab944b42f0c6
krishnam98 <sonikrishnam98@gmail.com>
2025-11-04 23:05:08 +0530

    Added API to get user By ID, Update User Profile

--- src/main/java/com/codeBuddy/codeBuddy_Backend/Controllers/UserController.java
+++ src/main/java/com/codeBuddy/codeBuddy_Backend/Controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.codeBuddy.codeBuddy_Backend.Controllers;
 
+import com.codeBuddy.codeBuddy_Backend.DTOs.UsersDTO;
 import com.codeBuddy.codeBuddy_Backend.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
@@ -17,6 +19,26 @@ public class UserController {
     @GetMapping("/getAll")
     public ResponseEntity<?> getAllUsers(){
         return userService.getUsers();
+    }
+
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/updatePersonalInfo")
+    public ResponseEntity<?>  updateUsersPersonalInfo(@RequestBody UsersDTO user){
+        return userService.updateUsersPersonalInfo(user);
+    }
+
+    @PutMapping("/updateSkills/{id}")
+    public ResponseEntity<?> updateSkills(@PathVariable Long id,@RequestBody List<String> skills){
+        return userService.updateSkills(id,skills);
+    }
+
+    @PutMapping("/updateInterests/{id}")
+    public ResponseEntity<?> updateInterests(@PathVariable Long id, @RequestBody List<String> interests){
+        return userService.updateInterests(id,interests);
     }
 
 
--- src/main/java/com/codeBuddy/codeBuddy_Backend/Services/UserService.java
+++ src/main/java/com/codeBuddy/codeBuddy_Backend/Services/UserService.java
@@ -8,15 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+
+    public UsersDTO convertToSingleUserDTO(Users user){
+        UsersDTO userDTO= new UsersDTO();
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setBio(user.getBio());
+        userDTO.setCollabs(user.getCollabs());
+        userDTO.setInterests(user.getInterests());
+        userDTO.setLocation(user.getLocation());
+        userDTO.setMobileNumber(user.getMobileNumber());
+        userDTO.setSkills(user.getSkills());
+        userDTO.setRating(user.getRating());
+
+        return userDTO;
+    }
 
     public List<UsersDTO> convertToUserDTO(List<Users> usersList){
         List<UsersDTO> usersDTOList= new ArrayList<>();
@@ -49,5 +68,78 @@ public class UserService {
            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    public ResponseEntity<?> getUserById(Long id) {
+        Optional optionalUser= userRepo.findById(id);
+        if(optionalUser.isPresent()){
+            UsersDTO usersDTO= convertToSingleUserDTO((Users)optionalUser.get());
+            return new ResponseEntity<>(usersDTO,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<?> updateUsersPersonalInfo(UsersDTO user) {
+        System.out.println("eneterd Backend");
+        Optional optional= userRepo.findById(user.getId());
+
+        if(optional.isPresent()){
+            try {
+                Users optionalUser= (Users)optional.get();
+                optionalUser.setName(user.getName());
+                optionalUser.setUsername(user.getUsername());
+                optionalUser.setMobileNumber(user.getMobileNumber());
+                optionalUser.setLocation((user.getLocation()));
+                optionalUser.setBio(user.getBio());
+                System.out.println(optionalUser);
+
+                userRepo.save(optionalUser);
+                return new ResponseEntity<>(optionalUser,HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("cannot update at this moment!",HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        }
+        else{
+            return new ResponseEntity<>("No user found with this ID", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<?> updateSkills(Long id, List<String> userSkills) {
+        Optional optional= userRepo.findById(id);
+
+        if(optional.isPresent()){
+            try{
+                Users optionalUser= (Users) optional.get();
+                optionalUser.setSkills(userSkills);
+                userRepo.save(optionalUser);
+                return new ResponseEntity<>(optionalUser,HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("cannot update at this moment", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }else{
+            return new ResponseEntity<>("No User found with this id", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<?> updateInterests(Long id, List<String> interests) {
+        Optional optional= userRepo.findById(id);
+
+        if(optional.isPresent()){
+            try{
+                Users optionalUser= (Users) optional.get();
+                optionalUser.setInterests(interests);
+
+                userRepo.save(optionalUser);
+                return new ResponseEntity<>(optionalUser,HttpStatus.OK);
+
+            } catch (Exception e) {
+                return new ResponseEntity<>("Cannot update at this moment", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }else{
+            return new ResponseEntity<>("No User found by this ID", HttpStatus.BAD_REQUEST);
+        }
     }
 }
